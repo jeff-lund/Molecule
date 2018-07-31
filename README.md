256fbd733afa5a8e86dc25d4a9fe951fb0d79987
jeff-lund <jdl3@pdx.edu>
2018-07-31 01:00:26 -0700

    Currently read from file specified by command line arg. Input is parsed to obtain chemical shift peaks and breakdown of the elemental analysis. Definitions added to README. Files holding molecular data in test_files. Should break up code so parsing is done in seperate functions. More error handling is needed, should check elements in elemental analysis against whitelist (C, N, O, H, Br, Cl, I). Parsing of elemental analysis cannot account for single elements.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Molecule
+
+C13 Chemical Shifts
+C=O (ketone) 		205-220
+C=O (aldehyde)		190-200
+C=O (acids/esters)	160-185
+C (aromatic)		125-150
+C=C (alkene)		115-140
+RCH2O-			50-90
+RCH2Cl			30-60
+RCH2NH2			30-65
+R3CH			25-35
+CH3CO-			20-50
+R2CH2			16-25
+RCH3			10-15
+
+File Input 
+[Elemental Analysis i.e C2H4O2]
+[Comma seperated chemical shifts i.e 162, 51]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+// Copyright (c) 2018 Jeff Lund
+use std::env::args;
+use std::fs::File;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+#[allow(dead_code)]
+enum Atom {
+    Hydrogen,
+    DoubleBond,
+    TripleBond,
+    CH3,
+    CH2,
+    CH,
+    CarboxylicAcid,
+    Ester,
+    Aldehyde,
+    Ketone,
+    Amide,
+    Aromatic,
+    Alkene,
+    Alkyne,
+    CO,
+    CN,
+    CCl,
+    CBr,
+}
+#[allow(dead_code)]
+struct Molecule {
+    kind: Atom,
+    bonds: [i32; 4],
+}
+
+fn main() -> std::io::Result<()> {
+    // Read file from std::args to buffer
+    let f = args().nth(1).expect("No file argument given");
+    let mut file = File::open(f)?;
+    let mut buffer =  String::new();
+    file.read_to_string(&mut buffer)?;
+    let mut b = buffer.lines(); // change this name later
+    // parsing elemental analysis
+    // breaks on single elements in formule i.e (CH4), needs error checking for valid elements
+    let chemical_formula = b.next().expect("invalid file");
+    let mut elements: Vec<&str> = chemical_formula.split(char::is_numeric).collect();
+    elements.retain(|e| e != &"");
+    let mut quantities: Vec<&str> = chemical_formula.split(char::is_alphabetic).collect();
+    quantities.retain(|e| e != &"");
+    let mut elemental_analysis = HashMap::new();
+    if elements.len() != quantities.len() {
+        panic!("elements and quantities do not match. Invalid chemical formula");
+    }
+    for (elem, quant) in elements.iter().zip(quantities.iter()) {
+        elemental_analysis.insert(elem, quant.parse::<i32>().expect("Not a number"));
+    }
+
+    // parsing chemical peaks
+    let mut chemical_peaks: Vec<i32> = Vec::new();
+    let peaks = b.next().expect("invalid file").split(',');
+    for p in peaks {
+        chemical_peaks.push(p.trim().parse::<i32>().expect("peaks has a non digit"));
+    }
+    println!("{:?}", elemental_analysis);
+    println!("{:?}", chemical_peaks);
+
+
+    Ok(())
 }
